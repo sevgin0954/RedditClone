074fc4b0acf3e55e745541ce3678100abc0ddbed
sevgin0954 <skateboardista_0954@abv.bg>
2019-03-29 17:46:58 +0200

    Add project files.

---
+++ RedditClone.Data/Data/RedditCloneDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedditClone.Data
+{
+    public class RedditCloneDbContext : IdentityDbContext
+    {
+        public RedditCloneDbContext(DbContextOptions<RedditCloneDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
